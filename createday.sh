1b2367c7c1fc511e8babb1dbaad512f81c7dbb3e
Tamer Aly <taly.dev75@gmail.com>
2020-12-10 19:22:41 -0500

    add a helper script to address creating a new day boilerplate

---
+++ createday.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne "1" ]; then
+    echo "day name must be supplied"
+    exit 1
+fi
+
+day="$1"
+
+mkdir $day 
+cd $day
+touch input.txt
+
+echo -e "
+import System.Environment
+
+solve1 :: String -> String
+solve1 = undefined
+
+solve2 :: String -> String
+solve2 = undefined
+
+mainWithArgs :: [String] -> IO ()
+mainWithArgs (\"1\":_) = interact solve1 >> putChar '\\\\n'
+mainWithArgs (\"2\":_) = interact solve2 >> putChar '\\\\n'
+mainWithArgs _       = error \"non-valid argument\"
+
+main :: IO ()
+main = getArgs >>= mainWithArgs
+" > Main.hs
+
+git add ..
