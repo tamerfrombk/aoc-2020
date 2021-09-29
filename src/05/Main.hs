eeb2b203a87211c44c8f7a06635d539774be7577
Tamer Aly <taly.dev75@gmail.com>
2021-09-29 18:28:35 -0400

    remove erroneous src/Main.hs

--- src/05/Main.hs
+++ src/05/Main.hs
@@ -1,31 +1,7 @@
 import System.Environment ( getArgs )
-data BoardingPass = BoardingPass {
-    rowV :: String,
-    colV :: String
-} deriving Show
-
-toBoardingPass :: String -> BoardingPass
-toBoardingPass s = BoardingPass { rowV = take 7 s, colV = drop 7 s }
-
-middle :: Fractional a => a -> a -> a
-middle lo hi = lo + ((hi - lo) / 2)
-
-calculateRow :: BoardingPass -> Int
-calculateRow (BoardingPass rows _) = calculateRow' rows a b
-    where calculateRow' [r]      mi mx = if r == 'F' then mi else mx
-          calculateRow' ('F':rs) mi mx = calculateRow' rs mi $ floor $ middle mi mx
-          calculateRow' ('B':rs) mi mx = calculateRow' rs (ceiling (middle mi mx)) mx
-          a = 0 :: Float
-          b = 127 :: Float
-
-calculateCol :: BoardingPass -> Int
-calculateCol _ = 7
-
-seatId :: BoardingPass -> Int
-seatId p = calculateRow p * 8 + calculateCol p 
 
 solve1 :: String -> String
-solve1 = show . maximum . map (seatId . toBoardingPass) . lines
+solve1 = undefined
 
 solve2 :: String -> String
 solve2 = undefined
