6cb3630fba0df9f59edbcd4593c563786eab3e42
Tamer Aly <taly.dev75@gmail.com>
2021-10-03 01:44:37 -0400

    5 pt 1

--- src/05/Main.hs
+++ src/05/Main.hs
@@ -1,7 +1,29 @@
 import System.Environment ( getArgs )
 
+move :: Char -> (Int, Int) -> (Int, Int)
+move c (l, u)
+    | c == 'F' || c == 'L' = (l, l + mid)
+    | c == 'B' || c == 'R' = if even (u - l) then (l + mid, u) else (l + mid + 1, u)
+    | otherwise            = error "undefined"
+    where mid = (u - l) `div` 2
+
+applyAll :: [a -> a] -> a -> a
+applyAll fs x = foldl (\ x f -> f x) x fs
+
+bsp :: (Int, Int) -> String -> Int
+bsp i s = let fns = map move s in fst $ applyAll fns i
+
+row :: String -> Int
+row s = bsp (0, 127) (take 7 s)
+
+col :: String -> Int
+col s = bsp (0, 7) (drop 7 s)
+
+boardingId :: String -> Int
+boardingId s = (row s * 8) + col s
+
 solve1 :: String -> String
-solve1 = undefined
+solve1 = show . maximum . map boardingId . lines
 
 solve2 :: String -> String
 solve2 = undefined
