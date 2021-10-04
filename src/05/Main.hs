3baf64c3841208ee6b5e8a03d8e19fa8fc905801
Tamer Aly <taly.dev75@gmail.com>
2021-10-03 20:36:14 -0400

    day 5 part 2

--- src/05/Main.hs
+++ src/05/Main.hs
@@ -1,4 +1,5 @@
 import System.Environment ( getArgs )
+import Data.List ( sort )
 
 move :: Char -> (Int, Int) -> (Int, Int)
 move c (l, u)
@@ -8,7 +9,7 @@ move c (l, u)
     where mid = (u - l) `div` 2
 
 applyAll :: [a -> a] -> a -> a
-applyAll fs x = foldl (\ x f -> f x) x fs
+applyAll fs x = foldl (flip ($)) x fs
 
 bsp :: (Int, Int) -> String -> Int
 bsp i s = let fns = map move s in fst $ applyAll fns i
@@ -26,7 +27,10 @@ solve1 :: String -> String
 solve1 = show . maximum . map boardingId . lines
 
 solve2 :: String -> String
-solve2 = undefined
+solve2 = show . diff2 . sort . map boardingId . lines
+    where diff2 (x:y:xs) = if y - x == 2 then x + 1 else diff2 xs
+          diff2 [x]      = x
+          diff2 []       = error "should never happen"
 
 mainWithArgs :: [String] -> IO ()
 mainWithArgs ("1":_) = interact solve1 >> putChar '\n'
