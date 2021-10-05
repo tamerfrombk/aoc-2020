5c5b031d8de8edae257de07f3fd94fe177fd1024
Tamer Aly <taly.dev75@gmail.com>
2021-10-05 15:59:32 -0400

    day 6, pt 2

--- src/06/Main.hs
+++ src/06/Main.hs
@@ -1,22 +1,27 @@
 import System.Environment ( getArgs )
 import Data.List ( sort, group, groupBy )
 
-type Group = String
+type Group = (String, Int)
 
 countYes :: Group -> Int
-countYes = length . group . sort
+countYes = length . group . sort . fst
 
 makeGroups :: String -> [Group]
 makeGroups = collect . lines
-    where collect :: [String] -> [String]
-          collect =  filter present . map concat . groupBy (\a b -> all present [a,b])
+    where collect :: [String] -> [Group]
+          collect = filter (\(_, l) -> l > 0) . groups . groupBy (\a b -> all present [a,b])
                 where present = not . null
+                      groups (y:ys) = (concat y, length y) : groups ys
+                      groups []     = []
 
 solve1 :: String -> String
 solve1 = show . sum . map countYes . makeGroups
 
+countAllYes :: Group -> Int
+countAllYes (s, l) = length . filter (\g -> length g == l ) $ group $ sort s
+
 solve2 :: String -> String
-solve2 = undefined
+solve2 = show . sum . map countAllYes . makeGroups
 
 mainWithArgs :: [String] -> IO ()
 mainWithArgs ("1":_) = interact solve1 >> putChar '\n'
